a4f8efccceb3cfa928211b875b4a2bb1aa603218
Philip Gough <philip.p.gough@gmail.com>
2020-01-27 12:02:19 +0000

    test:threescale: Unit test for computing metrics based on hierarchy

--- threescale/api/utilities_test.go
+++ threescale/api/utilities_test.go
@@ -1,6 +1,9 @@
 package api
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestHierarchy_DeepCopy(t *testing.T) {
 	h := make(Hierarchy)
@@ -102,5 +105,54 @@ func TestMetrics_DeepCopy(t *testing.T) {
 	clone["test"] = 3
 	if v, _ = original["test"]; v != 2 {
 		t.Error("unexpect value in original after modifying clone")
+	}
+}
+
+func TestMetrics_ComputeAffectedMetrics(t *testing.T) {
+	inputs := []struct {
+		name      string
+		original  Metrics
+		hierarchy Hierarchy
+		expect    Metrics
+	}{
+		{
+			name:      "Test empty hierarchy returns a copy",
+			original:  Metrics{"hits": 10, "test": 5},
+			hierarchy: Hierarchy{},
+			expect:    Metrics{"hits": 10, "test": 5},
+		},
+		{
+			name:     "Test childless parent unaffected",
+			original: Metrics{"hits": 10, "orphan": 5},
+			hierarchy: Hierarchy{
+				"other": []string{"child_one", "child_two"},
+			},
+			expect: Metrics{"hits": 10, "orphan": 5},
+		},
+		{
+			name:     "Test child metrics reflected onto known parent",
+			original: Metrics{"hits": 10, "orphan": 5, "child_one": 3},
+			hierarchy: Hierarchy{
+				"hits": []string{"child_one", "child_two"},
+			},
+			expect: Metrics{"hits": 13, "orphan": 5, "child_one": 3},
+		},
+		{
+			name:     "Test child metrics reflected onto unknown parent",
+			original: Metrics{"child_one": 3},
+			hierarchy: Hierarchy{
+				"hits": []string{"child_one", "child_two"},
+			},
+			expect: Metrics{"hits": 3, "child_one": 3},
+		},
+	}
+
+	for _, test := range inputs {
+		t.Run(test.name, func(t *testing.T) {
+			got := test.original.ComputeAffectedMetrics(test.hierarchy)
+			if !reflect.DeepEqual(got, test.expect) {
+				t.Errorf("unexpected metrics computed, expected %v, but got %v", test.expect, got)
+			}
+		})
 	}
 }
