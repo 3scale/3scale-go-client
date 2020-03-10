75fb5c922f63d5ecfce623d9f9e2130042b40e7e
Philip Gough <philip.p.gough@gmail.com>
2020-03-10 14:18:13 +0000

    threescale: Add helper for time conversion

--- threescale/helpers.go
+++ threescale/helpers.go
@@ -1,8 +1,19 @@
 package threescale
 
-import "github.com/3scale/3scale-go-client/threescale/api"
+import (
+	"time"
+
+	"github.com/3scale/3scale-go-client/threescale/api"
+)
+
+const timeLayout = "2006-01-02 15:04:05 -0700"
 
 // GetServiceID from Request
 func (r Request) GetServiceID() api.Service {
 	return r.Service
+}
+
+// FormatTimestamp from unix time to string formatting as understood by 3scale
+func FormatTimestamp(timestamp int64) string {
+	return time.Unix(timestamp, 0).Format(timeLayout)
 }
