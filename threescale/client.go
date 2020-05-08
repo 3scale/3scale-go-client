24092e57365fdf655eca51b8ec98fc0f62ec825d
Philip Gough <philip.p.gough@gmail.com>
2020-05-08 16:13:42 +0100

    lint: gofmt

--- threescale/client.go
+++ threescale/client.go
@@ -36,7 +36,7 @@ type AuthorizeResult struct {
 	// List of usage reports - list will be empty if no limits set
 	UsageReports api.UsageReports
 	// ErrorCode as returned by backend - see https://github.com/3scale/apisonator/blob/v2.96.2/docs/rfcs/error_responses.md
-	ErrorCode   string
+	ErrorCode string
 	// RejectionReason - human readable string explaining why authorization has not been granted
 	RejectionReason string
 	// RawResponse may be set by the underlying client implementation
